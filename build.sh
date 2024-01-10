8d9abef1dd5daac959c46f6eef15b68f083992e6
SimonWoodtli <xnasero@posteo.net>
2024-01-10 12:39:08 -0500

    🔨 rm empty lines

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
 set -uex pipefail
-
 apt-get update -y && apt-get install -y libevent-dev libncurses5-dev build-essential bison
 git clone http://github.com/tmux/tmux /tmp/tmux
 cd /tmp/tmux
